248c5e25f5080261716a57d4b6ac2940659ff2ee
TPicc27 <anpicc01@cardmail.louisville.edu>
2017-01-30 18:00:17 -0500

    initial commit

---
+++ CIS420CON/CIS420CON/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CIS420CON.Startup))]
+namespace CIS420CON
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
